e56dabf6388f5604af0bcdda8a01d50bf64721b1
Andy Grant <andy.grant@laterooms.com>
2015-09-13 11:22:23 +0100

    Passing sanity test to assert that test runner is doing its thing.

---
+++ CheckoutKata/CheckoutTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void Sanity_test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
