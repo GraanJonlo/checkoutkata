96b7353695678f5d6549c31bd1103ef5aa37ace6
Andy Grant <andy.grant@laterooms.com>
2015-09-13 11:29:59 +0100

    First proper failing test Scanning_an_A_gives_a_total_of_50

--- CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutTests.cs
@@ -10,5 +10,37 @@ namespace CheckoutKata
         {
             Assert.That(true, Is.True);
         }
+
+        [Test]
+        public void Scanning_an_A_gives_a_total_of_50()
+        {
+            Checkout checkout = new CheckoutBuilder().Build();
+
+            checkout.Scan("A");
+
+            var total = checkout.Total();
+            Assert.That(total, Is.EqualTo(50));
+        }
+    }
+
+    public class CheckoutBuilder
+    {
+        public Checkout Build()
+        {
+            return new Checkout();
+        }
+    }
+
+    public class Checkout
+    {
+        public int Total()
+        {
+            return 0;
+        }
+
+        public void Scan(string sku)
+        {
+            // no op
+        }
     }
 }
