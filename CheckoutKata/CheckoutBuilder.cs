ff73a4a2e854638d305dc7af83311f9d8f299d66
Andy Grant <andy.grant@laterooms.com>
2015-09-20 14:58:36 +0100

    Refactor - Simplified CheckoutBuilder

--- CheckoutKata/CheckoutBuilder.cs
+++ CheckoutKata/CheckoutBuilder.cs
@@ -1,38 +1,28 @@
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CheckoutKata
 {
     public class CheckoutBuilder
     {
-        private IListenForSkus _priceLookup = new PriceLookup(new Dictionary<string, int>(4)
-        {
-            {"A", 50},
-            {"B", 30},
-            {"C", 20},
-            {"D", 15}
-        });
-
-        private readonly List<IListenForSkus> _discountTrackers = new List<IListenForSkus>();
+        private readonly IList<IListenForSkus> _skuListeners = new List<IListenForSkus>();
 
         private readonly IKeepTotal _total = new InMemoryTotal();
 
         public CheckoutBuilder WithPriceLookup(IListenForSkus priceLookup)
         {
-            _priceLookup = priceLookup;
+            _skuListeners.Add(priceLookup);
             return this;
         }
 
         public CheckoutBuilder WithDiscountTracker(IListenForSkus discountTracker)
         {
-            _discountTrackers.Add(discountTracker);
+            _skuListeners.Add(discountTracker);
             return this;
         }
 
         public Checkout Build()
         {
-            var skuListeners = new List<IListenForSkus> {_priceLookup}.Concat(_discountTrackers).ToList();
-            return new Checkout(skuListeners, _total);
+            return new Checkout(_skuListeners, _total);
         }
     }
 }
